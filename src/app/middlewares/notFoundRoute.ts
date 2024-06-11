5f21b06faf1eb2d4eb273494be922abbf9bb68f2
Golam Rabbani <golamrabbani9373@gmail.com>
2024-06-11 23:14:03 +0600

    Add Status Code For Route Not Found

--- src/app/middlewares/notFoundRoute.ts
+++ src/app/middlewares/notFoundRoute.ts
@@ -7,8 +7,8 @@ import httpStatus from 'http-status'
 const notFoundRoute = (req: Request, res: Response, next: NextFunction) => {
   return res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: 'API Not Found !!',
-    error: '',
+    StatusCode: httpStatus.NOT_FOUND,
+    message: 'Route Not Found !',
   })
 }
 
