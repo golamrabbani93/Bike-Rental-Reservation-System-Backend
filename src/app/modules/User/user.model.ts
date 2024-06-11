95edb6b0b9e8eec917455fa14e44ed49d3034dbf
Golam Rabbani <golamrabbani9373@gmail.com>
2024-06-12 00:51:34 +0600

    password hash complete with bcrypt

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,5 +1,7 @@
+import bcrypt from 'bcrypt'
 import { TUser } from './user.interface'
 import { Schema, model } from 'mongoose'
+import config from '../../config'
 
 const UserSchema = new Schema<TUser>(
   {
@@ -38,6 +40,18 @@ const UserSchema = new Schema<TUser>(
     timestamps: true,
   },
 )
+
+UserSchema.pre('save', async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  )
+
+  next()
+})
 
 // * Create User Model
 export const User = model<TUser>('User', UserSchema)
